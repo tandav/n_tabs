bc9817ee7ef315b7bf75cd34c396ab4ff754d1d3
Alexander Rodionov <alx1dr@gmail.com>
2021-04-14 23:26:12 +0300

    add stats webpage

--- n_tabs.py
+++ n_tabs.py
@@ -1,5 +1,7 @@
 import datetime
 import macos
+import json
+import subprocess
 
 n_tabs = macos.n_tabs()
 
@@ -9,6 +11,13 @@ with open('n_tabs.csv') as fd:
     n_tabs_old = int(line.split(',')[1])
 
 if n_tabs != n_tabs_old:
+    now = datetime.datetime.now()
+
     with open('n_tabs.csv', 'a') as fd:
-        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}", n_tabs, sep=',', file=fd)
+        print(f"{now.strftime('%Y-%m-%d %H:%M:%S')}", n_tabs, sep=',', file=fd)
+
+    with open('/Users/tandav/Downloads/n_tabs_json/n_tabs.json', 'w') as fd:
+        json.dump({'n_tabs' : n_tabs, 'updated_at': int(now.timestamp() * 1000)}, fd)
+        
+    subprocess.run(('sh', 'update.sh'), cwd='/Users/tandav/Downloads/n_tabs_json')
     # macos.notification(text=f'{n_tabs}', title='n_tabs')
