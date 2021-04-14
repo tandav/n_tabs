1d0eba0a16ceaf61d70b424e30120e07a985c98a
Alexander Rodionov <alx1dr@gmail.com>
2021-04-14 23:48:25 +0300

    better stats

--- n_tabs.py
+++ n_tabs.py
@@ -2,13 +2,13 @@ import datetime
 import macos
 import json
 import subprocess
+import statistics
 
 n_tabs = macos.n_tabs()
 
 with open('n_tabs.csv') as fd:
-    for line in fd:
-        pass
-    n_tabs_old = int(line.split(',')[1])
+    tabs = [int(line.split(',')[1]) for line in fd]
+n_tabs_old = tabs[-1]
 
 if n_tabs != n_tabs_old:
     now = datetime.datetime.now()
@@ -17,7 +17,12 @@ if n_tabs != n_tabs_old:
         print(f"{now.strftime('%Y-%m-%d %H:%M:%S')}", n_tabs, sep=',', file=fd)
 
     with open('/Users/tandav/Downloads/n_tabs_json/n_tabs.json', 'w') as fd:
-        json.dump({'n_tabs' : n_tabs, 'updated_at': int(now.timestamp() * 1000)}, fd)
+        json.dump({
+            'n_tabs' : n_tabs,
+            'updated_at': int(now.timestamp() * 1000),
+            'mean': int(statistics.mean(tabs)),
+            'std': int(statistics.stdev(tabs)),
+        }, fd)
         
     subprocess.run(('sh', 'update.sh'), cwd='/Users/tandav/Downloads/n_tabs_json')
     # macos.notification(text=f'{n_tabs}', title='n_tabs')
