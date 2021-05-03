35a7977723244ee3ce17bb0f9765046f41cbf47a
Alexander Rodionov <alx1dr@gmail.com>
2021-05-03 21:21:47 +0300

    add whitelist host stats

--- n_tabs.py
+++ n_tabs.py
@@ -3,15 +3,33 @@ import macos # https://github.com/tandav/dotfiles/blob/master/bin/macos.py
 import json
 import subprocess
 import statistics
+import collections
+from urllib.parse import urlparse
 
-n_tabs, n_windows = macos.n_tabs(n_windows=True)
+whitelist = set(open('whitelist.txt').read().split())
+
+tabs = macos.tabs()
+
+n_tabs = 0
+n_windows = 0
+hosts = collections.Counter()
+
+for window in tabs:
+    n_windows += 1
+    for title, url in window:
+        n_tabs += 1
+        host = urlparse(url).hostname
+        if host not in whitelist:
+            host = 'etc'
+        hosts[host] += 1
+
 
 with open('n_tabs.csv') as fd:
-    tabs = [int(line.split(',')[1]) for line in fd]
-n_tabs_old = tabs[-1]
+    n_tabs_history = [int(line.split(',')[1]) for line in fd]
+n_tabs_old = n_tabs_history[-1]
 
 if n_tabs != n_tabs_old:
-    tabs.append(n_tabs)
+    n_tabs_history.append(n_tabs)
     now = datetime.datetime.now()
 
     with open('n_tabs.csv', 'a') as fd:
@@ -19,15 +37,15 @@ if n_tabs != n_tabs_old:
 
     with open('/Users/tandav/Downloads/n_tabs_json/n_tabs.json', 'w') as fd:
         json.dump({
-            'n_tabs' : n_tabs,
+            'n_tabs': n_tabs,
             'n_windows': n_windows,
             'updated_at': int(now.timestamp() * 1000),
-            'max': max(tabs),
-            'min': min(tabs),
-            'mean': int(statistics.mean(tabs)),
-            'std': int(statistics.stdev(tabs)),
-            'median': int(statistics.median(tabs)),
+            'max': max(n_tabs_history),
+            'min': min(n_tabs_history),
+            'mean': int(statistics.mean(n_tabs_history)),
+            'std': int(statistics.stdev(n_tabs_history)),
+            'median': int(statistics.median(n_tabs_history)),
+            'hosts': hosts,
         }, fd)
-        
+
     subprocess.run(('sh', 'update.sh'), cwd='/Users/tandav/Downloads/n_tabs_json')
-    # macos.notification(text=f'{n_tabs}', title='n_tabs')
